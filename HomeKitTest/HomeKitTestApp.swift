9d2242b6f7dcd7b3516128f74747d7be40c47721
Marco Carmona <elbuki@users.noreply.github.com>
2023-06-19 12:57:46 -0600

    Initial Commit

---
+++ HomeKitTest/HomeKitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeKitTestApp.swift
+//  HomeKitTest
+//
+//  Created by Marco Carmona on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct HomeKitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
